4b1673f94e96a34eace26dd077b9e46338cf4358
prathamn1 <prathamnagaria@gmail.com>
2022-08-24 13:06:47 +0530

    added allowed hosts

--- wisdompets/settings.py
+++ wisdompets/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j1nsafv69mi(%dd899kcb%+&0+z3zuse64&)(o+=be9x#v^#s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # changed for hosting
-# DEBUG_PROPAGATE_EXCEPTIONS = True
+DEBUG = False # changed for hosting
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
-# ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS = ['127.0.0.1','https://pet-adoption-site.herokuapp.com/']
+# ALLOWED_HOSTS=[]
 
 
 # Application definition
